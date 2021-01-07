aac493d39eabeb41168c9ca55854db2b59d8dba1
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-01-07 23:22:55 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let Question={
+    posX:750,
+    posY:canvas.height/2,
+};
+
+let Answer={
+    posX:150,
+    posY:canvas.height/2,
+}
+
+let command={
+    
+}
+
+let QueImage = new Image();
+let AnsImage = new Image();
+
+let upImage = new Image();
+let downImage = new Image();
+let leftImage = new Image();
+let rightImage = new Image();
+
+let Q_B_Image = new Image();
+let A_B_Image = new Image();
+
+QueImage.src= "./image/Question.png";
+AnsImage.src = "./image/Answer.png";
+
+upImage.src="./image/up.png";
+downImage.src="./image/down.png";
+leftImage.src="./image/left.png";
+rightImage.src="./image/right.png";
--- script/graphic.js
+++ script/graphic.js
@@ -1,19 +1,13 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let Que = new Image();
-let Ans = new Image();
-
-Que.src= "./image/Question.png";
-Ans.src = "./image/Answer.png";
-
-Que.onload=()=>
+QueImage.onload=()=>
 {
-    ctx.drawImage(Que,200,5,70,50*Que.height/Que.width);
+    ctx.drawImage(QueImage,Question.posX,Question.posY);
 }
 
-Ans.onload=()=>
+AnsImage.onload=()=>
 {
-    ctx.drawImage(Ans,100,5,70,50*Ans.height/Ans.width);
+    ctx.drawImage(AnsImage,Answer.posX,Answer.posY);
 }
 
